7c47ea8279e7ad759781e6557e8cca67c45b16ad
Benjamin <benjamin.barvas@gmail.com>
2023-05-30 15:51:51 +0200

    Finished project

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const container = document.querySelector(".container");
-
-function createDiv(height, width) {
-    for (let i = 0; i < height; i++) {
-        let heightDiv = document.createElement("div");
-        container.appendChild(heightDiv);
-        heightDiv.classList.add("heightDiv", "div")
-        for (let j = 0; j < width; j++) {
-            let widthDiv = document.createElement("div");
-            heightDiv.appendChild(widthDiv);
-            widthDiv.classList.add("widthDiv", "div")
+function createDiv(width, height) {
+    for (let i = 0; i < width; i++) {
+        let widthDiv = document.createElement("div");
+        container.appendChild(widthDiv);
+        widthDiv.classList.add("widthDiv", "div")
+        for (let j = 0; j < height; j++) {
+            let heightDiv = document.createElement("div");
+            widthDiv.appendChild(heightDiv);
+            heightDiv.classList.add("heightDiv", "div")
+            heightDiv.style.height = `${(700 - height) / height}px`;
+            heightDiv.style.width = `${(700 - width) / width}px`;
         }
-    }
+    }   
 }
 
 function getRandomColor() {
@@ -25,6 +26,43 @@ container.addEventListener("mouseover", (event) => {
     setTimeout(() => {
         event.target.style.backgroundColor = "";
     }, 500)
+});
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+    let width;
+    while(true) {
+        width = prompt("How many boxes horizontally do you want?", 16);
+        if((width > 0 && width <= 100) || width === null) {    
+            break ;
+        } else if (width < 0) {
+            alert("Can't make negative number of boxes");
+        } else if (width > 100) {
+            alert("Too large number, max is 100");
+        } else { 
+            alert("Invalid entry. Try a number from 1 to 100");
+        }
+    }
+    let height;
+    while(true) {
+        height = prompt("How many boxes horizontally do you want?", 16);
+        if((height > 0 && height <= 100) || height === null) {    
+            break;
+        } else if (height < 0) {
+            alert("Can't make negative number of boxes");
+        } else if (height > 100) {
+            alert("Too large number, max is 100");
+        } else { 
+            alert("Invalid entry. Try a number from 1 to 100");             
+        }
+    }
+    if ((width > 0 && width <= 100) && (height > 0 && height <= 100)) {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+        createDiv(width, height);
+    }
 });
 
 createDiv(16, 16);
